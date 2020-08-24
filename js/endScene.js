04a7b3c656741d5c74aa795f9ea75887ef70bda4
JayMassey98 <49314718+JayMassey98@users.noreply.github.com>
2020-08-24 16:26:29 +0100

    Create endScene.js

---
+++ js/endScene.js
@@ -0,0 +1,31 @@
+class endScene extends Phaser.Scene {
+    
+    constructor() {
+        
+        super({ key: "endScene" });
+    }
+
+    create(){
+
+        this.background = this.add.tileSprite(0, 0, config.width, config.height, "background");
+        this.background.setOrigin(0, 0);
+        
+        this.restartButton = this.add.sprite(config.width / 2, config.height / 2, "buttonRestart");
+        this.restartButton.setInteractive();
+        this.restartButton.setScale(2, 2);
+        
+        this.restartButton.on("pointerup", function() { this.scene.start("menu"); }, this);
+
+        this.title = this.add.text(30, 30, "Game Over.", {
+            fontFamily: 'monospace',
+            fontSize: 40,
+            fontStyle: 'bold',
+            color: '#ffffff',
+            align: 'center'
+        });
+        
+        this.title.setOrigin(0, 0);
+
+    }
+
+}
